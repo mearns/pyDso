71d8f7a02bfcb247be05d7c849af62c0db10770c
Brian Mearns <bmearns@ieee.org>
2015-06-25 22:32:53 -0400

    Added some more tests for propagating errors and complete.

--- tests.py
+++ tests.py
@@ -54,7 +54,7 @@ def test_map():
     subj.on_next(3)
     subj.on_next(4)
 
-    ok_(uut.join_all(4))
+    ok_(uut.join_all(1))
 
     eq_(observer.get_events(), (0, 2, 4, 6, 8))
     ok_(not observer.is_complete())
@@ -62,11 +62,83 @@ def test_map():
     subj.on_error('e')
     subj.on_error('f')
     ok_(uut.join_all(1))
+    #Because we aren't propagating errors.
+    eq_(len(observer.get_errors()), 0)
 
     subj.on_complete()
     ok_(uut.join_all(1))
     #Because we aren't propagating complete
     ok_(not observer.is_complete())
+
+def test_propagate_errors():
+
+    subj = Subject()
+    uut = DerivedObservable(subj, True)
+    observer = uut.subscribe(CollectingObsever())
+    
+    subj.on_next(0)
+    subj.on_next(1)
+    subj.on_next(2)
+    subj.on_error('e')
+    subj.on_error('f')
+    subj.on_next(3)
+    subj.on_error('g')
+
+    ok_(uut.join_all(1))
+    eq_(observer.get_events(), (0,1,2,3))
+    eq_(observer.get_errors(), ('e', 'f', 'g'))
+    ok_(not observer.is_complete())
+
+    subj.on_complete()
+    ok_(uut.join_all(1))
+    #Still not propagating complete.
+    ok_(not observer.is_complete())
+
+def test_propagate_complete():
+
+    subj = Subject()
+    uut = DerivedObservable(subj, False, True)
+    observer = uut.subscribe(CollectingObsever())
+    
+    subj.on_next(0)
+    subj.on_next(1)
+    subj.on_next(2)
+    subj.on_error('e')
+    subj.on_error('f')
+    subj.on_next(3)
+    subj.on_error('g')
+
+    ok_(uut.join_all(1))
+    eq_(observer.get_events(), (0,1,2,3))
+    eq_(len(observer.get_errors()), 0)
+    ok_(not observer.is_complete())
+
+    subj.on_complete()
+    ok_(uut.join_all(1))
+    ok_(observer.is_complete())
+
+def test_propagate_erros_and_complete():
+
+    subj = Subject()
+    uut = DerivedObservable(subj, True, True)
+    observer = uut.subscribe(CollectingObsever())
+    
+    subj.on_next(0)
+    subj.on_next(1)
+    subj.on_next(2)
+    subj.on_error('e')
+    subj.on_error('f')
+    subj.on_next(3)
+    subj.on_error('g')
+
+    ok_(uut.join_all(1))
+    eq_(observer.get_events(), (0,1,2,3))
+    eq_(observer.get_errors(), ('e', 'f', 'g'))
+    ok_(not observer.is_complete())
+
+    subj.on_complete()
+    ok_(uut.join_all(1))
+    ok_(observer.is_complete())
 
 
     
